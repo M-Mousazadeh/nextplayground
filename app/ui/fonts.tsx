dff491180ddaf8603acd8e8e85091d35bf373904
Mohammadreza Mousazadeh <mrr.mousazadeh@gmail.com>
2025-05-18 22:13:31 +0330

    first commit

---
+++ app/ui/fonts.tsx
@@ -0,0 +1,7 @@
+import {Inter} from 'next/font/google';
+import {Lusitana} from 'next/font/google';
+import { Vazirmatn } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin']});
+export const lusitana = Lusitana({weight:['700']});
+export const vazir = Vazirmatn({subsets: ['arabic'], weight: ['400']})
